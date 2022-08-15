2823d64a765dd1364dca03cb5c131e3509434dcb
poncho678 <vincent.tollens@googlemail.com>
2022-08-15 19:55:54 +0200

    added darkmode toggle + localstorage

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,21 @@ document.addEventListener(
   },
   false
 );
+
+// ####################### DARKMODE TOGGLE #######################
+const themeToggle = document.querySelector(".btn-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+themeToggle.addEventListener("click", function (e) {
+  e.preventDefault();
+  document.body.classList.toggle("dark-theme");
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
