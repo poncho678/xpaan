d63b8e7a3219e970382704a0258954135984cfeb
poncho678 <vincent.tollens@googlemail.com>
2022-08-18 21:44:06 +0200

    feat: naive implementation of img upload

--- routes/item.routes.js
+++ routes/item.routes.js
@@ -4,18 +4,56 @@ const itemRouter = express.Router({ mergeParams: true });
 const CollectionModel = require("../models/Collection.model");
 const UserModel = require("../models/User.model");
 const ItemModel = require("../models/Item.model");
+const uploadMiddleware = require("../middleware/cloudinary");
 
 // create Item
 itemRouter.get("/create", (req, res) => {
   const { collectionId } = req.params;
+
   res.render("item/create", { collectionId });
 });
-itemRouter.post("/create", async (req, res) => {
-  console.log(req.params);
-});
+itemRouter.post(
+  "/create-img",
+  uploadMiddleware.single("img"),
+  async (req, res) => {
+    const { collectionId } = req.params;
+    const { type } = req.query;
+    const { path: img } = req.file;
+    const { title, text } = req.body;
+
+    const createItem = await ItemModel.create({
+      title,
+      text,
+      img,
+      type,
+      collectionId,
+    });
+
+    await CollectionModel.findByIdAndUpdate(collectionId, {
+      $push: { items: createItem._id },
+    });
+
+    res.redirect(`/collection/${collectionId}/item/${createItem._id}`);
+  }
+);
 
 // view Item
-itemRouter.get("/:itemId", async (req, res) => {});
+itemRouter.get("/:itemId", async (req, res) => {
+  const { itemId, collectionId } = req.params;
+
+  const theItem = ItemModel.findById(itemId);
+
+  if (!theItem) {
+    console.log("item not found");
+    res.status(400).redirect(`/collection/${collectionId}`);
+  }
+
+  console.log("item found, but no page available");
+
+  //   find item in model...
+
+  res.render("item/view");
+});
 
 // edit Item
 itemRouter.get("/:itemId/edit", (req, res) => {});
