4f008de1a091d2b55a9c8bbf901814abec7479d9
poncho678 <vincent.tollens@googlemail.com>
2022-08-26 18:48:09 +0200

    feat: update password

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -7,10 +7,10 @@ const {
   Types: { ObjectId },
 } = require("mongoose");
 
-const saltRounds = 14;
-
-// Require the User model in order to interact with the database
+// Require the User model, CollectionModel, ItemModel in order to interact with the database
 const User = require("../models/User.model");
+const CollectionModel = require("../models/Collection.model");
+const ItemModel = require("../models/Item.model");
 
 // Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
 const isLoggedOut = require("../middleware/isLoggedOut");
@@ -18,30 +18,30 @@ const isLoggedIn = require("../middleware/isLoggedIn");
 
 async function checkIfUserExists(req, res, next) {
   const user = await User.findById(req.session.user._id);
-
   if (!user) {
     return res.redirect("/");
   }
-
-  req.user = user;
-
+  res.locals.user = user;
   next();
 }
 
+// perform check on every profile call
 profileRouter.use(checkIfUserExists);
 
-profileRouter.get("/", isLoggedIn, (req, res) => {
-  const { username } = req.user;
+// main profile page
+profileRouter.get("/", isLoggedIn, async (req, res) => {
+  const { username } = res.locals.user;
   return res.render("profile/profile", { username });
 });
 
-profileRouter.get("/update-username", isLoggedIn, (req, res) => {
-  const { username } = req.user;
+// update username
+profileRouter.get("/update-username", isLoggedIn, async (req, res) => {
+  const { username } = res.locals.user;
   return res.render("profile/update-username", { username });
 });
 
 profileRouter.post("/update-username", isLoggedIn, async (req, res) => {
-  const { username, _id } = req.user;
+  const { username, _id } = res.locals.user;
   const { newUsername } = req.body;
 
   if (!newUsername) {
@@ -68,7 +68,6 @@ profileRouter.post("/update-username", isLoggedIn, async (req, res) => {
       errorMessage: `${newUsername} is already taken`,
     });
   }
-  console.log(newUsername);
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
@@ -78,20 +77,79 @@ profileRouter.post("/update-username", isLoggedIn, async (req, res) => {
       new: true,
     }
   );
-  req.session.user.username = updatedUser.username;
-  return res.render("profile/profile", { username: newUsername });
+
+  return res.render("profile/profile", { username: updatedUser.username });
 });
 
+// update password
 profileRouter.get("/update-password", isLoggedIn, (req, res) => {
   return res.render("profile/update-password");
 });
+profileRouter.post("/update-password", isLoggedIn, async (req, res) => {
+  const { user } = res.locals;
 
-profileRouter.post("/update-password", isLoggedIn, (req, res) => {
-  const { oldPassword, newPassword, confirmNewPassword } = req.body;
-  return res.render("profile/update-password");
+  const {
+    currentPassword = "",
+    newPassword = "",
+    confirmNewPassword = "",
+  } = req.body;
+
+  if (
+    !currentPassword ||
+    newPassword.length < 8 ||
+    confirmNewPassword.length < 8 ||
+    newPassword !== confirmNewPassword
+  ) {
+    //
+    return res.status(400).render("profile/update-password", {
+      errorMessage: "Fill every input correctly",
+    });
+  }
+
+  if (currentPassword === newPassword) {
+    return res.status(400).render("profile/update-password", {
+      errorMessage: "Please enter a new password",
+    });
+  }
+
+  const isSamePassword = bcrypt.compareSync(currentPassword, user.password);
+  if (!isSamePassword) {
+    return res.status(400).render("settings/update-password", {
+      user,
+      errorMessage: "That is not your password",
+    });
+  }
+  const salt = bcrypt.genSaltSync(15);
+  const hashedPassword = await bcrypt.hashSync(newPassword, salt);
+
+  await User.findByIdAndUpdate(user._id, { password: hashedPassword });
+
+  return res.redirect("/");
 });
 
+// delete user
 profileRouter.get("/delete-user", isLoggedIn, (req, res) => {
+  const { username, _id } = res.locals.user;
+  return res.render("profile/delete-user", { username });
+});
+
+profileRouter.post("/delete-user", isLoggedIn, (req, res) => {
+  const { username, _id } = res.locals.user;
+  const { confirmUsername } = req.body;
+
+  if (!confirmUsername) {
+    return res.status(400).render("profile/delete-user", {
+      username,
+      errorMessage: "Please confirm your Username...",
+    });
+  }
+  if (confirmUsername !== username) {
+    return res.status(400).render("profile/delete-user", {
+      username,
+      errorMessage: `“${confirmUsername}” is not your username. Try again.`,
+    });
+  }
+
   return res.render("profile/delete-user");
 });
 
