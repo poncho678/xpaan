9d6820375c849457aed405507bf4887d756e751e
poncho678 <vincent.tollens@googlemail.com>
2022-08-27 15:59:36 +0200

    added export/imprt for multiple consts

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -2,10 +2,12 @@ const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const { v2: cloudinary } = require("cloudinary");
 
+const { cloudinaryFolder } = require("../utils/consts");
+
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "xpaan-images",
+    folder: cloudinaryFolder,
   },
 });
 
--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,4 @@
 const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/xpaan";
+const cloudinaryFolder = "xpaan-images";
 
-module.exports = MONGO_URI;
+module.exports = { MONGO_URI, cloudinaryFolder };
